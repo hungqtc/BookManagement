d9ca549d4f32b7b3920340d97b0d568b6163a070
hungqtc <hungqtc97@gmail.com>
2020-02-05 17:22:28 +0700

    update CORS

--- src/main/java/com/hung/config/SecurityConfig.java
+++ src/main/java/com/hung/config/SecurityConfig.java
@@ -51,7 +51,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 	CorsConfigurationSource corsConfigurationSource() {
 		CorsConfiguration configuration = new CorsConfiguration();
 		configuration.setAllowedOrigins(Arrays.asList("*"));
-		configuration.setAllowedMethods(Arrays.asList("GET","POST"));
+		configuration.setAllowedMethods(Arrays.asList("*"));
 		UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
 		source.registerCorsConfiguration("/**", configuration);
 		return source;
