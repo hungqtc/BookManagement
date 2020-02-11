b189da26325271c322d1528a0098e97f64857e7e
hungqtc <hungqtc97@gmail.com>
2020-02-11 19:51:45 +0700

    done CORS

--- src/main/java/com/hung/config/SecurityConfig.java
+++ src/main/java/com/hung/config/SecurityConfig.java
@@ -48,14 +48,21 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 	}
 	
 	@Bean
-	CorsConfigurationSource corsConfigurationSource() {
-		CorsConfiguration configuration = new CorsConfiguration();
-		configuration.setAllowedOrigins(Arrays.asList("http://localhost:3000"));
-		configuration.setAllowedMethods(Arrays.asList("*"));
-		UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
-		source.registerCorsConfiguration("/**", configuration);
-		return source;
-	}
+    public CorsConfigurationSource corsConfigurationSource() {
+        final CorsConfiguration configuration = new CorsConfiguration();
+        configuration.setAllowedOrigins(Arrays.asList("*"));
+        configuration.setAllowedMethods(Arrays.asList("HEAD",
+            "GET", "POST", "PUT", "DELETE", "PATCH"));
+        // setAllowCredentials(true) is important, otherwise:
+        // The value of the 'Access-Control-Allow-Origin' header in the response must not be the wildcard '*' when the request's credentials mode is 'include'.
+        configuration.setAllowCredentials(true);
+        // setAllowedHeaders is important! Without it, OPTIONS preflight request
+        // will fail with 403 Invalid CORS request
+        configuration.setAllowedHeaders(Arrays.asList("Authorization", "Cache-Control", "Content-Type"));
+        final UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", configuration);
+        return source;
+    }
 	
 	@Bean(name = BeanIds.AUTHENTICATION_MANAGER)
 
@@ -71,9 +78,9 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
 	@Override
 	protected void configure(HttpSecurity http) throws Exception {
-		http.csrf().disable();
-		http.cors().configurationSource(corsConfigurationSource());
-		http.authorizeRequests()
+		http.csrf().disable()
+				.cors().configurationSource(corsConfigurationSource()) 
+		    .and().authorizeRequests()
 			.antMatchers("/api/login").permitAll()
 			.antMatchers(HttpMethod.GET,"/api/books").permitAll() 
 			.antMatchers(HttpMethod.GET,"/api/comments").permitAll() 
